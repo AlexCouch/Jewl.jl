3bbea577e724ffe32ef23c046205a3f8a9a4071b
alexsofsafe <alcouch65@gmail.com>
2023-11-26 20:24:58 -0800

    Fixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,14 @@ using Test
 using PyPlot
 
 @testset "Load" begin
-    bubble_sort = Jewl.Load("../data/dump.json")
+    bubble_sort = Jewl.Load("../data/bubble_sort.json")
     js = Jewl.CollectVariable(bubble_sort, "j")
     x, y = ([time for (time, j) in js], [j for (time, j) in js])
     plot(x, y)
 end
 
 @testset "Sample" begin
-    bubble_sort = Jewl.Load("../data/dump.json")
+    bubble_sort = Jewl.Load("../data/bubble_sort.json")
     js = Jewl.Sample(bubble_sort, "outer_loop", "inner_loop", "j")
     boxplot(js)
 end
